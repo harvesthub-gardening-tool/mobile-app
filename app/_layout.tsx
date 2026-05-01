87b82bddb8c50930bbb12c91cf1191b232a8061f
PierreWTH <wietrichpierre@gmail.com>
2026-05-01 18:56:12 +0200

    add hub setup modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,11 +4,15 @@ import { AuthProvider } from "./context/AuthContext";
 export default function RootLayout() {
     return (
         <AuthProvider>
-            <Stack
-                screenOptions={{
-                    headerShown: false,
-                }}
-            />
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen
+                    name="hub-setup"
+                    options={{
+                        presentation: "transparentModal",
+                        animation: "none",
+                    }}
+                />
+            </Stack>
         </AuthProvider>
     );
 }
